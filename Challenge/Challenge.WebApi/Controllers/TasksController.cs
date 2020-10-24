4ea5957f028f64cf71dce63a689a1e8ed2124cc8
Hossein Zahed <hzahed@outlook.com>
2020-10-24 12:00:19 +0200

    TaskDto added for controller input.

---
+++ Challenge/Challenge.WebApi/Controllers/TasksController.cs
@@ -0,0 +1,32 @@
+﻿using Challenge.WebApi.Interfaces;
+using Challenge.WebApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Challenge.WebApi.Controllers
+{
+    [ApiController]
+    [Produces("application/json")]
+    [Route("api/tasks")]
+    public class TasksController : ControllerBase
+    {
+        private readonly IBackgroundQueue<string> _queue;
+
+        public TasksController(IBackgroundQueue<string> queue)
+        {
+            _queue = queue;
+        }
+
+        [HttpPost]
+        public IActionResult Post(TaskDto dto)
+        {
+            if (dto != null && !string.IsNullOrWhiteSpace(dto.InputText))
+            {
+                _queue.Enqueue(dto.InputText);
+                return Accepted();
+            }
+
+            ModelState.AddModelError("Error", "Input text is required!");
+            return BadRequest(ModelState);
+        }
+    }
+}
---
+++ Challenge/Challenge.WebApi/Models/TaskDto.cs
@@ -0,0 +1,7 @@
+﻿namespace Challenge.WebApi.Models
+{
+    public class TaskDto
+    {
+        public string InputText { get; set; }
+    }
+}
