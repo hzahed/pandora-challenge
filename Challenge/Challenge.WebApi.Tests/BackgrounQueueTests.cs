1c1b8478f4c963d97d4cecf93c71ff0a9ae05f7d
Hossein Zahed <hzahed@outlook.com>
2020-10-24 13:11:28 +0200

    2 tests added for background queue.

---
+++ Challenge/Challenge.WebApi.Tests/BackgrounQueueTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading.Tasks;
+using Challenge.WebApi.Services;
+using FakeItEasy;
+using Xunit;
+
+namespace Challenge.WebApi.Tests
+{
+    public class BackgrounQueueTests
+    {
+        [Fact]
+        public void EnqueueNotNullItemShouldAddToQueue()
+        {
+            // Arrange
+            var item = "test";
+            var backgroundQueue = A.Fake<BackgroundQueue<string>>();
+            
+            // Act
+            backgroundQueue.Enqueue(item);
+
+            // Assert
+            Assert.Equal(item, backgroundQueue.Dequeue());
+        }
+
+        [Fact]
+        public void EnqueueNullItemShouldThrowException()
+        {
+            // Arrange
+            var backgroundQueue = A.Fake<BackgroundQueue<string>>();
+
+            // Act & Assert
+            Assert.Throws<ArgumentNullException>(() => backgroundQueue.Enqueue(null));
+        }
+    }
+}
