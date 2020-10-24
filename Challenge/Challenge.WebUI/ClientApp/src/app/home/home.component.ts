c24335eee3c47ec41c4bf3a6a9731610c2e8d7e2
Hossein Zahed <hzahed@outlook.com>
2020-10-24 10:38:23 +0200

    Form added to component to send input text.

--- Challenge/Challenge.WebUI/ClientApp/src/app/home/home.component.ts
+++ Challenge/Challenge.WebUI/ClientApp/src/app/home/home.component.ts
@@ -1,8 +1,24 @@
 import { Component } from '@angular/core';
+import { DataService } from '../services/data.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
 export class HomeComponent {
+  inputText: string = null;
+
+  constructor(private dataService: DataService) {
+  }
+
+  send() {
+    this.dataService.post('tasks', {text: this.inputText}).subscribe(
+      (result: any) => {
+
+      },
+      (error) => {
+        console.error(error);
+      }
+    );
+  }
 }
