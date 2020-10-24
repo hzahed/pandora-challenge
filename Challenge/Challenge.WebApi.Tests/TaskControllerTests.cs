a3565f811eafb1c7f8b472a0b20603d8ad983b35
Hossein Zahed <hzahed@outlook.com>
2020-10-24 13:01:40 +0200

    2 Tests added for controller.

--- Challenge/Challenge.WebApi.Tests/TaskControllerTests.cs
+++ Challenge/Challenge.WebApi.Tests/TaskControllerTests.cs
@@ -1,4 +1,9 @@
-using System;
+using Challenge.WebApi.Controllers;
+using Challenge.WebApi.Interfaces;
+using Challenge.WebApi.Models;
+using FakeItEasy;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
 using Xunit;
 
 namespace Challenge.WebApi.Tests
@@ -6,9 +11,37 @@ namespace Challenge.WebApi.Tests
     public class TaskControllerTests
     {
         [Fact]
-        public void Test1()
+        public void PostInputTextShouldReturnAccepted()
         {
+            // Arrange
+            var backgroundQueue = A.Fake<IBackgroundQueue<string>>();
+            var tasksController = new TasksController(backgroundQueue);
+            var dto = new TaskDto { InputText = "Test 1" };
 
+            // Act
+            var responseResult = tasksController.Post(dto) as ObjectResult;
+
+            // Assert
+            Assert.IsType<AcceptedResult>(responseResult);
+            Assert.NotNull(responseResult.StatusCode);
+            Assert.Equal(HttpStatusCode.Accepted, (HttpStatusCode)responseResult.StatusCode);
+        }
+
+        [Fact]
+        public void PostNullShouldReturnBadRequest()
+        {
+            // Arrange
+            var backgroundQueue = A.Fake<IBackgroundQueue<string>>();
+            var tasksController = new TasksController(backgroundQueue);
+            var dto = new TaskDto { InputText = null };
+
+            // Act
+            var responseResult = tasksController.Post(dto) as ObjectResult;
+
+            // Assert
+            Assert.IsType<BadRequestObjectResult>(responseResult);
+            Assert.NotNull(responseResult.StatusCode);
+            Assert.Equal(HttpStatusCode.BadRequest, (HttpStatusCode)responseResult.StatusCode);
         }
     }
 }
